0d97300f705b4d9607c613667aaaf418e02564cb
Your Name <you@example.com>
2026-02-10 13:03:46 -0500

    meh

--- insomnia.c
+++ insomnia.c
@@ -2,13 +2,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+
+#define SANITY_DIR "/etc/sanity.d/"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Usage: insomnia [reboot|off|die]\n");
+        printf("Usage: insomnia [reboot|off|die|enable <script>|disable <script>]\n");
         return 1;
     }
 
+    // Handle enable/disable (File System logic)
+    if (argc == 3) {
+        char path[256];
+        snprintf(path, sizeof(path), "%s%s", SANITY_DIR, argv[2]);
+
+        struct stat st;
+        if (stat(path, &st) != 0) {
+            perror("Insomnia: Script not found");
+            return 1;
+        }
+
+        if (strcmp(argv[1], "enable") == 0) {
+            chmod(path, st.st_mode | S_IXUSR | S_IXGRP | S_IXOTH);
+            printf("Insomnia: Enabled %s\n", argv[2]);
+            return 0;
+        } 
+        else if (strcmp(argv[1], "disable") == 0) {
+            chmod(path, st.st_mode & ~(S_IXUSR | S_IXGRP | S_IXOTH));
+            printf("Insomnia: Disabled %s\n", argv[2]);
+            return 0;
+        }
+    }
+
+    // Handle signals to Sanity (FIFO logic)
     int fd = open("/run/sanity.fifo", O_WRONLY);
     if (fd < 0) {
         perror("Insomnia: Cannot reach Sanity (is the FIFO there?)");
@@ -20,6 +47,7 @@ int main(int argc, char *argv[]) {
         printf("Insomnia: Sent termination signal to PID 1.\n");
     } else {
         write(fd, argv[1], strlen(argv[1]));
+        printf("Insomnia: Sent '%s' to Sanity.\n", argv[1]);
     }
 
     close(fd);
